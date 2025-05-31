b0fe3980babfccb6a490cbab86ac9852c493c12b
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2025-05-31 19:24:29 +0200

    Add Home and Contact pages with interactive 3D models.
    - Created `Home.jsx` featuring 3D interactive models and responsive adjustments.
    - Implemented `Contact.jsx` with a functional contact form and animation effects.
    - Added reusable components: `CTA`, `Footer`, and `Alert`.
    - Introduced models for `Fox`, `Sky`, `Island`, and `Plane`, leveraging glTF assets.
    - Enhanced constants file with skills, experiences, social links, and projects.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import './index.css'
+import { Footer, Navbar } from "./components";
+import { About, Contact, Home, Projects } from "./pages";
 
-function App() {
+const App = () => {
+    return (
+        <main className='bg-slate-300/20'>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route
+                        path='/*'
+                        element={
+                            <>
+                                <Routes>
+                                    <Route path='/about' element={<About />} />
+                                    <Route path='/projects' element={<Projects />} />
+                                    <Route path='/contact' element={<Contact />} />
+                                </Routes>
+                                <Footer />
+                            </>
+                        }
+                    />
+                </Routes>
+            </Router>
+        </main>
+    );
+};
 
-  return (
-    <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  )
-}
-
-export default App
+export default App;
---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+
+import { arrow } from "../assets/icons";
+
+const HomeInfo = ({ currentStage }) => {
+    if (currentStage === 1)
+        return (
+            <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+                Hi, I'm
+                <span className='font-semibold mx-2 text-white'>Gabriel</span>
+                👋
+                <br />
+                A Software Engineer from Italy🇮🇹
+            </h1>
+        );
+
+    if (currentStage === 2) {
+        return (
+            <div className='info-box'>
+                <p className='font-medium sm:text-xl text-center'>
+                    Worked with many companies <br /> and picked up many skills along the way
+                </p>
+
+                <Link to='/about' className='neo-brutalism-white neo-btn'>
+                    Learn more
+                    <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+                </Link>
+            </div>
+        );
+    }
+
+    if (currentStage === 3) {
+        return (
+            <div className='info-box'>
+                <p className='font-medium text-center sm:text-xl'>
+                    Led multiple projects to success over the years. <br /> Curious about the impact?
+                </p>
+
+                <Link to='/projects' className='neo-brutalism-white neo-btn'>
+                    Visit my portfolio
+                    <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+                </Link>
+            </div>
+        );
+    }
+
+    if (currentStage === 4) {
+        return (
+            <div className='info-box'>
+                <p className='font-medium sm:text-xl text-center'>
+                    Got an idea or just need a curious mind to think with?<br />
+                    I'm here — not just as a dev.
+                </p>
+
+                <Link to='/contact' className='neo-brutalism-white neo-btn'>
+                    Let's connect
+                    <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+                </Link>
+            </div>
+
+        );
+    }
+
+    return null;
+};
+
+export default HomeInfo;
