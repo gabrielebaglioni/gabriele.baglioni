0d39965843329a4711361d4cb5ae166cfcf59c31
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2025-06-02 23:25:14 +0200

    Update Navbar links for improved clarity and user navigation
    - Renamed "About" to "About my work" for a clearer purpose.
    - Rebranded "Projects" to "About me" to better reflect content focus.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
             </NavLink>
             <nav className='flex text-lg gap-7 font-medium'>
                 <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-white-100" }>
-                    About
+                    About my work
                 </NavLink>
                 <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-white-100"}>
-                    Projects
+                    About me
                 </NavLink>
             </nav>
         </header>
