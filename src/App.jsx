c4040d3b0dbed15fc1d8f300298b5fbdf7051a37
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2025-06-05 18:23:34 +0200

    add abiout me page try

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import { Footer, Navbar } from "./components";
-import { About, Contact, Home, Projects,  } from "./pages";
+import { About, Contact, Home, AboutMeWrapper,  } from "./pages";
 import About2 from "./pages/About2.jsx";
+import ParallaxBackground from "./components/parallaxBackground.jsx";
 
 const App = () => {
     return (
-        <main className='animated-bg min-h-screen'>
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route
-                        path='/*'
-                        element={
-                            <>
-                                <Routes>
-                                    <Route path='/about' element={<About2 />} />
-                                    <Route path='/projects' element={<Projects />} />
-                                    <Route path='/contact' element={<Contact />} />
-                                </Routes>
-                                <Footer />
-                            </>
-                        }
-                    />
-                </Routes>
-            </Router>
+        <main className=' min-h-screen'>
+                <ParallaxBackground />
+               
+                    <Router>
+                        <Navbar />
+                        <Routes>
+                            <Route path='/' element={<Home />} />
+                            <Route
+                                path='/*'
+                                element={
+                                    <>
+                                        <Routes>
+                                            <Route path='/about' element={<About2 />} />
+                                            <Route path='/projects' element={<AboutMeWrapper />} />
+                                            <Route path='/contact' element={<Contact />} />
+                                        </Routes>
+                                        <Footer />
+                                    </>
+                                }
+                            />
+                        </Routes>
+                    </Router>
         </main>
     );
 };
---
+++ src/pages/about Me/AboutMeWrapper.jsx
@@ -0,0 +1,44 @@
+
+import { CTA } from "../../components/index.js";
+import HeroAboutMe from "./heroAboutMe.jsx";
+
+
+const AboutMeWrapper = () => {
+  return (
+    <section className='container mx-auto max-w-7xl'>
+        <HeroAboutMe />
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.      
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+
+
+
+
+
+
+
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.  
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.  
+        </p>
+      <hr className='border-slate-200' />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default AboutMeWrapper;
