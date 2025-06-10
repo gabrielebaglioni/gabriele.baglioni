ddb4b2822fe51380bb3a14072e7e301daba4b2b8
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2025-06-10 12:16:32 +0200

    Adjust `AndroidError` component height to 50% for improved layout scaling.

--- src/components/AndroidError.jsx
+++ src/components/AndroidError.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AndroidError = () => (
   <div style={{
     width: '100%',
-    height: '50%',
+    height: '25%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
